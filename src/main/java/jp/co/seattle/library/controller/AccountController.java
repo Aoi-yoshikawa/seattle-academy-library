91008fca3c4d7d0630eaf71b8529a9a9e7f99152
yoshikawaaoi <yoshikawaaoi@yoshikaaoinoair>
2023-04-24 15:05:16 +0900

    タスク修正2

--- src/main/java/jp/co/seattle/library/controller/AccountController.java
+++ src/main/java/jp/co/seattle/library/controller/AccountController.java
@@ -63,7 +63,7 @@ public class AccountController {
 				return "createAccount";
 			}
 		} else {
-			model.addAttribute("パスワードは8文字以上かつ半角英数字に設定してください。");
+			model.addAttribute("errorMessage", "パスワードは8文字以上かつ半角英数字に設定してください。");
 			return "createAccount";
 		}
 	}
